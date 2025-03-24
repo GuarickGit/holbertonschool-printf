9a41e8cf71b6e434f449bf5039c0596b26c33673
GuarickGit <10615@holbertonstudents.com>
2025-03-24 17:07:48 +0100

    update all functions and add descriptions

--- specifiers.c
+++ specifiers.c
@@ -1,12 +1,26 @@
 #include "main.h"
 
+/**
+ * print_char - affiche un caractère
+ * @arguments: va_list contenant le caractère à afficher
+ *
+ * Return: nombre de caractères affichés (toujours 1)
+ */
 int print_char(va_list arguments)
 {
 	char c = va_arg(arguments, int);
+
 	_putchar(c);
-	return (0);
+
+	return (1);
 }
 
+/**
+ * print_string - affiche une chaîne de caractères
+ * @arguments: va_list contenant la string à afficher
+ *
+ * Return: nombre de caractères affichés
+ */
 int print_string(va_list arguments)
 {
 	int i;
@@ -14,13 +28,30 @@ int print_string(va_list arguments)
 
 	char *str = va_arg(arguments, char *);
 
+	/* si la string est NULL, le contenu de str devient "(null)" */
 	if (!str)
-		return (-1);
+		str = "(null)";
 
+	/* on parcourt chaque caractère de la string */
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		counter += _putchar(str);
+		_putchar(str[i]);
+		counter++;
 	}
 
+	return (counter);
+}
 
+/**
+ * print_percent - affiche un pourcentage (%)
+ * @arguments: va_list non utilisé, mais nécessaire pour la cohérence de
+ * la structure
+ *
+ * Return: nombre de caractères affichés (toujours 1)
+ */
+int print_percent(va_list arguments)
+{
+	(void)arguments;
+	_putchar('%');
+	return (1);
 }

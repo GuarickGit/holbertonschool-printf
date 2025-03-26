d933eed82fc4a45321bef7b1f3f15cf75739cdd3
GuarickGit <10615@holbertonstudents.com>
2025-03-26 09:39:13 +0100

    add function print_integer

--- specifiers.c
+++ specifiers.c
@@ -64,3 +64,55 @@ int print_percent(va_list arguments)
 
 	return (1); /* Sinon, on a affiché 1 caractère */
 }
+
+#include "main.h"
+
+/**
+ * print_integer - affiche un entier signé (pour %d ou %i)
+ * @arguments: va_list contenant l'entier à afficher
+ *
+ * Return: nombre de caractères affichés
+ */
+int print_integer(va_list arguments)
+{
+	/* utilisation de long pour éviter un overflow si on a INT_MIN */
+	long n;
+	int count = 0, i = 0;
+
+	/* on prépare un tableau pour stocker les chiffres à l'envers */
+	/* suffisant pour contenir tous les chiffres d’un long */
+	char waiting_numbers[20];
+
+	/* récupération de l'entier depuis la liste d'arguments (toujours en int) */
+	n = (long)va_arg(arguments, int);
+
+	/* si le nombre est négatif, on affiche '-' et on le rend positif */
+	if (n < 0)
+	{
+		_putchar('-'); /* on affiche le signe moins */
+		count++; /* on compte ce caractère affiché */
+		n = -n; /* on passe en positif (en sécurité car n est un long) */
+	}
+	/* si le nombre est zéro, on l'affiche tout de suite */
+	if (n == 0)
+	{
+		_putchar('0'); /* on affiche le chiffre 0 */
+		return (count + 1); /* on retourne le nombre total de caractères */
+	}
+
+	/* On extrait les chiffres un à un (du dernier au premier) */
+	while (n > 0)
+	{
+		/* on convertit chaque chiffre en caractère */
+		waiting_numbers[i++] = (n % 10) + '0';
+		n /= 10; /* On enlève ce chiffre du nombre */
+	}
+	/* on affiche les chiffres dans le bon ordre (en les lisant à l'envers) */
+	while (i--)
+	{
+		_putchar(waiting_numbers[i]); /* on affiche le caractère actuel */
+		count++; /* on incrémente le compteur de caractères */
+	}
+	/* on retourne le nombre total de caractères affichés */
+	return (count);
+}

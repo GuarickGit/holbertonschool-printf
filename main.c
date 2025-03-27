32cfc0fa063a50052da94242abb9a2973b3f60e9
Helvlaska <ccastan@live.fr>
2025-03-27 18:25:02 +0100

    main.c update file : Add tests to print_binary function

--- main.c
+++ main.c
@@ -20,8 +20,14 @@ int main(void)
 	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("[%b]\n", 98);
-	printf("[%b]\n", 98);
+	_printf("Binary: [%b]\n", 0);
+	printf("Binary: [%b]\n", 0);
+	_printf("Binary: [%b]\n", 1);
+	printf("Binary: [%b]\n", 1);
+	_printf("Binary: [%b]\n", 345);
+	printf("Binary: [%b]\n", 345);
+	_printf("Binary: décimal : [%d], binaire :[%b]\n", 10, 10);
+	printf("Binary: décimal : [%d], binaire :[%b]\n", 10, 10);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
 	_printf("Unsigned:[%u]\n", ui);
